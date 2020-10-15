88e5da9c3808aefda7bca3a3f7398f80dcf6ba1b
LP2X0X <phamquanglong.work@gmail.com>
2020-10-15 15:25:54 +0700

    Code da sua lai

--- Guess_Number_Game.py
+++ Guess_Number_Game.py
@@ -1,26 +1,31 @@
-print('--Xin chao cac ban den voi Guess Number Game--')
-print('\nBan co 5 luot doan')
-import random
+#Import section
+import sys
 import numpy as np
+
+#Gioi thieu
+print('--Xin chao cac ban den voi Guess Number Game--\nBan co 5 luot doan')
 number = np.random.randint(low =1,high = 100)
-guess=input('Nhap so ban doan trong khoang tu (1,100): ')
-guess = int(guess)
+
+#De debug
+print(number) #Bo comment cau nay de debug
+
+#Main program
+guess=int(input('Nhap so ban doan trong khoang tu (1,100): ')) #Them int ben ngoai de chuyen sang dang int luon cho input
 i=1
 while(i!= 6):
+    #Doan dung
 	if(guess == number):
-		print('\n --CHUC MUNG BAN DA DOAN DUNG--')
-		i = 6
+		print('\n       --CHUC MUNG BAN DA DOAN DUNG--')
+		sys.exit() #Exit chuong trinh luon neu doan dung
+	#Doan lon hon
 	elif(guess>number):
 		print('\n Nhap so nho hon! ')
-		guess= input('\n Nhap so: ')
-		guess = int(guess)
-		i = i+1
+		guess= int(input('\n Nhap so: '))
+		i = i + 1
+	#Doan nho hon
 	elif(guess<number):
 		print('\n Nhap so lon hon! ')	
-		guess= input('\n Nhap so: ')
-		guess = int(guess)
-		i = i+1
-if(guess==  number):
-	print('\n --CHUC MUNG BAN DA DOAN DUNG--')
-else:
-	print('\n --Ban da loss-- ')		
+		guess= int(input('\n Nhap so: '))
+		i = i + 1
+
+print('\n       --Ban da loss-- ')
